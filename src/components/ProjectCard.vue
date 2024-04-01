80ff38ab4e6f78385d5a4c400b1817a511820b3e
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-04-01 22:15:30 +0300

    fix phone view of works

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,7 +9,7 @@ defineProps<ProjectCardProps>();
 </script>
 
 <template>
-    <a class="w-60 h-48 mb-4 border shadow-md transition hover:duration-300 hover:ease-in-out hover:-translate-y-1.5" :href="project.projectUrl">
+    <a class="w-40 h-44 lg:w-60 lg:h-48 mb-4 border shadow-md transition hover:duration-300 hover:ease-in-out hover:-translate-y-1.5" :href="project.projectUrl">
         <div class="flex min-h-24 min-w-24 justify-center items-center">
             <img :src="`/${project.imageUrl}.png`" :alt="project.imageUrl">
         </div>
