9cb1585c9b7cb094d433632e52120644514fbfec
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-04-01 22:00:47 +0300

    change image paths

--- src/components/CompanyInfo.vue
+++ src/components/CompanyInfo.vue
@@ -13,7 +13,7 @@ defineProps<CompanyInfoProps>();
     <div class="flex justify-center mt-5">
         <div class="flex-row items-center justify-center">
             <a class="group flex h-10 w-20 items-center justify-center" :href="websiteLink" target="_blank">
-                <img class="transition duration-300 ease-in-out group-hover:-translate-y-1.5" loading="lazy" height="140" width="90" :src="`../assets/img/${logoName}.png`" :alt="alt">
+                <img class="transition duration-300 ease-in-out group-hover:-translate-y-1.5" loading="lazy" height="140" width="90" :src="`/${logoName}.png`" :alt="alt">
             </a>
             <p class="font-extralight text-center text-[10px]">{{ date }}</p>
         </div>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -11,7 +11,7 @@ defineProps<ProjectCardProps>();
 <template>
     <a class="w-60 h-48 mb-4 border shadow-md transition hover:duration-300 hover:ease-in-out hover:-translate-y-1.5" :href="project.projectUrl">
         <div class="flex min-h-24 min-w-24 justify-center items-center">
-            <img :src="`/src/assets/img/${project.imageUrl}.png`" :alt="project.imageUrl">
+            <img :src="`/${project.imageUrl}.png`" :alt="project.imageUrl">
         </div>
         <p class="mx-2">{{ project.description }}</p>
     </a>
